9df4f497bcdadac8a1b55c8c548f1b2bb0332b39
Nate Long <long.nathaniel@gmail.com>
2013-10-09 12:11:40 -0700

    Added ability to easily embed videos easily

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ YUI.add("yulb", function(Y) {
     "use strict";
 
     var launchers     = Y.all(".yulb-launcher"),
+        embeds        = Y.all(".yulb-embed"),
         overlayTmpl   = "<div class=\"yulb-overlay\" style=\"width:{width}px; height: {height}px;\"></div>",
         containerTmpl = "<div class=\"yulb-container\" style=\"width:{width}px; height:{height}px;\"></div>",
+        closeBtnTmpl  = "<div class=\"yulb-close\">close</div>",
         iframeTmpl    = "<iframe type=\"text/html\" width=\"{width}\" height=\"{height}\" src=\"//www.youtube.com/embed/{vidid}?{params}\" frameborder=\"0\" />",
         defaults      = {
             width  : 800,
@@ -20,57 +22,73 @@ YUI.add("yulb", function(Y) {
 
     Y.yulb = {
         defaults  : defaults,
-        launchers : launchers
+        launchers : launchers,
+        embeds    : embeds
     };
 
-    if(launchers.isEmpty()) {
-        return;
-    }
+    if(!launchers.isEmpty()) {
+        launchers.each(function(launcher) {
+            launcher.on("click", function(e) {
+                e.preventDefault();
 
-    launchers.each(function(launcher) {
-        launcher.on("click", function(e) {
-            e.preventDefault();
+                var popupData = {
+                        vidid  : launcher.getData("vidid"),
+                        width  : +launcher.getData("vidwidth") || defaults.width,
+                        height : +launcher.getData("vidheight") || defaults.height,
+                        params : Y.Object.keys(defaults.params).map(function(key) {
+                                    return key + "=" + defaults.params[key];
+                                 }).join("&")
+                    },
+                    overlayData = {
+                        width  : Y.DOM.winWidth(),
+                        height : Y.DOM.winHeight()
+                    },
+                    containerData = {
+                        width  : popupData.width,
+                        height : popupData.height
+                    },
+                    eventData = {
+                        launcher : launcher,
+                        vidid    : popupData.vidid
+                    },
+                    overlay   = Y.Node.create(Y.Lang.sub(overlayTmpl, overlayData)),
+                    container = Y.Node.create(Y.Lang.sub(containerTmpl, containerData)).appendTo(overlay);
+                
+                Y.Node.create(closeBtnTmpl).appendTo(container);
+                Y.Node.create(Y.Lang.sub(iframeTmpl, popupData)).appendTo(container);
+                overlay.appendTo(document.body);
 
-            var popupData = {
-                    vidid  : launcher.getData("vidid"),
-                    width  : +launcher.getData("vidwidth") || defaults.width,
-                    height : +launcher.getData("vidheight") || defaults.height,
-                    params : Y.Array.map(Y.Object.keys(defaults.params), function(key) {
-                                return key + "=" + defaults.params[key];
-                             }).join("&")
-                },
-                overlayData = {
-                    width  : Y.DOM.winWidth(),
-                    height : Y.DOM.winHeight()
-                },
-                containerData = {
-                    width  : popupData.width,
-                    height : popupData.height
-                },
-                eventData = {
-                    launcher : launcher,
-                    vidid    : popupData.vidid
-                },
-                overlay   = Y.Node.create(Y.Lang.sub(overlayTmpl, overlayData)),
-                container = Y.Node.create(Y.Lang.sub(containerTmpl, containerData)).appendTo(overlay);
-            
-            Y.Node.create(Y.Lang.sub(iframeTmpl, popupData)).appendTo(container);
-            overlay.appendTo(document.body);
+                Y.fire("yulb:open", eventData);
 
-            Y.fire("yulb:open", eventData);
+                overlay.on("click", function() {
+                    Y.fire("yulb:close", eventData);
+                    overlay.remove(true);
+                });
 
-            overlay.on("click", function() {
-                Y.fire("yulb:close", eventData);
-                overlay.remove(true);
-            });
-
-            Y.on("windowresize", function() {
-                overlay.setStyles({
-                    "width"  : Y.DOM.winWidth(),
-                    "height" : Y.DOM.winHeight()
+                Y.on("windowresize", function() {
+                    overlay.setStyles({
+                        "width"  : Y.DOM.winWidth(),
+                        "height" : Y.DOM.winHeight()
+                    });
                 });
             });
         });
-    });
+    }
 
-}, "1.0.0", { requires: [ "node", "event", "array-extras" ] });
+    if(!embeds.isEmpty()) {
+        embeds.each(function(embed) {
+            var embedParams = Y.merge(defaults.params, { enablejsapi : 1 }),
+                popupData = {
+                    vidid  : embed.getData("vidid"),
+                    width  : +embed.getData("vidwidth") || defaults.width,
+                    height : +embed.getData("vidheight") || defaults.height,
+                    params : Y.Object.keys(embedParams).map(function(key) {
+                                return key + "=" + defaults.params[key];
+                             }).join("&")
+                };
+
+            Y.Node.create(Y.Lang.sub(iframeTmpl, popupData)).appendTo(embed);
+        });
+    }
+
+}, "1.0.0", { requires: [ "node", "event" ] });
