29ffab9ba756059bf452573684aad51e87f93d0e
Nate Long <long.nathaniel@gmail.com>
2013-11-21 14:22:44 -0800

    Moved some stuff around
    now supports muted, autoplaying videos

--- main.js
+++ main.js
@@ -1,42 +1,93 @@
-/*global document, YUI */
+/*jshint browser:true */
+/*global document, YUI, YT */
 YUI.add("yulb", function(Y) {
     "use strict";
 
-    var launchers     = Y.all(".yulb-launcher"),
-        embeds        = Y.all(".yulb-embed"),
-        overlayTmpl   = "<div class=\"yulb-overlay\" style=\"width:{width}px; height: {height}px;\"></div>",
-        containerTmpl = "<div class=\"yulb-container\" style=\"width:{width}px; height:{height}px;\"></div>",
-        closeBtnTmpl  = "<div class=\"yulb-close\">close</div>",
-        iframeTmpl    = "<iframe type=\"text/html\" width=\"{width}\" height=\"{height}\" src=\"//www.youtube.com/embed/{vidid}?{params}\" frameborder=\"0\" />",
-        defaults      = {
+    var launchers  = Y.all(".yulb-launcher"),
+        embeds     = Y.all(".yulb-embed"),
+        apiUrl     = "https://www.youtube.com/iframe_api",
+        tmpl       = {
+            overlay   : "<div class=\"yulb-overlay\" style=\"width:{width}px; height: {height}px;\"></div>",
+            container : "<div class=\"yulb-container\" style=\"width:{width}px; height:{height}px;\"></div>",
+            closeBtn  : "<div class=\"yulb-close\">close</div>",
+            iframe    : "<iframe type=\"text/html\" id=\"{id}\" width=\"{width}\" height=\"{height}\" src=\"//www.youtube.com/embed/{vidid}?{params}\" frameborder=\"0\" />"
+        },
+        defaults   = {
             width  : 800,
             height : 600,
+            volumeRampTime : 1000,
             params : {
-                autoplay       : 1,
+                autoplay       : 0,
                 modestbranding : 1,
                 showinfo       : 0,
                 rel            : 0,
-                autohide       : 1
+                autohide       : 1,
+                version        : 3,
+                enablejsapi    : 1
             }
         };
 
-    Y.yulb = {
-        defaults  : defaults,
-        launchers : launchers,
-        embeds    : embeds
-    };
+    if(!launchers.isEmpty() || !embeds.isEmpty()) {
+        Y.Get.js(apiUrl, function(err) {
+            if(err) {
+                Y.log("Youtube API couldn't load", "error");
+            }
+        });
+    }
 
-    if(!launchers.isEmpty()) {
+    function addApiControls(player, controller) {
+        var id   = player.get("id"),
+            mute = controller.getData("mute") === "true",
+            startVolume,
+            volumeInterval;
+
+        function initialize(e) {
+            function volumeStep() {
+                var currentVolume = e.target.getVolume();
+
+                if(currentVolume < startVolume) {
+                    e.target.setVolume(currentVolume + 1);
+                } else {
+                    clearInterval(volumeInterval);
+                }
+            }
+
+            if(mute) {
+                startVolume = e.target.getVolume();
+                e.target.setVolume(0);
+
+                player.on("mouseenter", function() {
+                    volumeInterval = setInterval(volumeStep, defaults.volumeRampTime / startVolume);
+                });
+            }
+        }
+
+        new YT.Player(id, {
+            events: {
+                "onReady": initialize
+            }
+        });
+    }
+
+    function setupLaunchers() {
+        if(launchers.isEmpty()) {
+            return;
+        }
+
         launchers.each(function(launcher) {
             launcher.on("click", function(e) {
                 e.preventDefault();
 
-                var popupData = {
+                var playerParams = Y.merge(defaults.params, {
+                        autoplay : (launcher.getData("autoplay") === "true" ? 1 : 0)
+                    }),
+                    popupData = {
+                        id     : Y.guid(),
                         vidid  : launcher.getData("vidid"),
                         width  : +launcher.getData("vidwidth") || defaults.width,
                         height : +launcher.getData("vidheight") || defaults.height,
-                        params : Y.Array.map(Y.Object.keys(defaults.params), function(key) {
-                                    return key + "=" + defaults.params[key];
+                        params : Y.Array.map(Y.Object.keys(playerParams), function(key) {
+                                    return key + "=" + playerParams[key];
                                  }).join("&")
                     },
                     overlayData = {
@@ -51,12 +102,13 @@ YUI.add("yulb", function(Y) {
                         launcher : launcher,
                         vidid    : popupData.vidid
                     },
-                    overlay   = Y.Node.create(Y.Lang.sub(overlayTmpl, overlayData)),
-                    container = Y.Node.create(Y.Lang.sub(containerTmpl, containerData)).appendTo(overlay);
-                
-                Y.Node.create(closeBtnTmpl).appendTo(container);
-                Y.Node.create(Y.Lang.sub(iframeTmpl, popupData)).appendTo(container);
+                    overlay   = Y.Node.create(Y.Lang.sub(tmpl.overlay, overlayData)),
+                    container = Y.Node.create(Y.Lang.sub(tmpl.container, containerData)).appendTo(overlay),
+                    player    = Y.Node.create(Y.Lang.sub(tmpl.iframe, popupData)).appendTo(container);
+
+                Y.Node.create(tmpl.closeBtn).appendTo(container);
                 overlay.appendTo(document.body);
+                addApiControls(player, launcher);
 
                 Y.fire("yulb:open", eventData);
 
@@ -75,20 +127,39 @@ YUI.add("yulb", function(Y) {
         });
     }
 
-    if(!embeds.isEmpty()) {
+    function setupEmbeds() {
+        if(embeds.isEmpty()) {
+            return;
+        }
+
         embeds.each(function(embed) {
-            var embedParams = Y.merge(defaults.params, { enablejsapi : 1 }),
-                popupData = {
+            var embedParams = Y.merge(defaults.params, {
+                    autoplay : (embed.getData("autoplay") === "true" ? 1 : 0)
+                }),
+                embedData = {
+                    id     : Y.guid(),
                     vidid  : embed.getData("vidid"),
                     width  : +embed.getData("vidwidth") || defaults.width,
                     height : +embed.getData("vidheight") || defaults.height,
                     params : Y.Object.keys(embedParams).map(function(key) {
-                                return key + "=" + defaults.params[key];
+                                return key + "=" + embedParams[key];
                              }).join("&")
-                };
+                },
+                player = Y.Node.create(Y.Lang.sub(tmpl.iframe, embedData)).appendTo(embed);
 
-            Y.Node.create(Y.Lang.sub(iframeTmpl, popupData)).appendTo(embed);
+            addApiControls(player, embed);
         });
     }
 
-}, "1.0.0", { requires: [ "node", "event", "array-extras" ] });
+    window.onYouTubeIframeAPIReady = function onYouTubeIframeAPIReady() {
+        setupLaunchers();
+        setupEmbeds();
+    };
+
+}, "1.0.0", {
+    requires: [
+        "node",
+        "event",
+        "array-extras"
+    ]
+});
